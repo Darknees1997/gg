6b901b4e7bccc2cf53ff10bfb5f8c32c9e256de5
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-03 21:25:05 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from 'angularfire2';
-import { environment } from '../environments/environment'; 
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore'; 
 
 @NgModule({
   declarations: [AppComponent],
