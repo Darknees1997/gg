204f33850614874a5040f7b409fa51d04172b892
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-03 22:10:28 -0400

    Update todo.service.ts

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -10,7 +10,35 @@ import { TaskI } from '../models/task.interface';
 export class TodoService {
   private todosCollection: AngularFirestoreCollection<TaskI>;
   private todos: Observable<TaskI[]>;
-  
 
-  constructor() { }
+
+  constructor(db : AngularFirestore) { 
+    this.todosCollection = db.collection<TaskI>('todos');
+    this.todos = this.todosCollection.snapshotChanges().pipe(map(
+      actions => {
+        return actions.map(a =>{
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id; 
+          return {id, ...data};
+        });
+      }
+    ));
+  }
+
+  getTodos(){
+     return this.todos;
+  }
+
+  getTodo(id: string){
+     return this.todosCollection.doc<TaskI>(id).valueChanges();
+  }
+
+  updateTodo(todo:TaskI, id: string){
+     return this.todosCollection.doc(id).update(todo);
+  }
+
+  addTodo(todo:TaskI){
+      return this.todosCollection.add(todo);
+  }
+
 }
