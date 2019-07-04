53b041b22cde5ac52decf16b5623e24d8ba89572
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-03 21:16:57 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDtmHHtZ0Yi-Ztp8_3-oKOt5a6GltarywA",
+    authDomain: "vjuegos-61114.firebaseapp.com",
+    databaseURL: "https://vjuegos-61114.firebaseio.com",
+    projectId: "vjuegos-61114",
+    storageBucket: "vjuegos-61114.appspot.com",
+    messagingSenderId: "945880931278",
+    appId: "1:945880931278:web:a9632a834b09cb2f"
+  }
 };
 
 /*
